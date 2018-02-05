730383d0aa83c35b7617fe6504ef635fcc33219e
ohazyi <291045048@qq.com>
2018-02-05 09:03:15 +0800

    :sparkles: finsh collect button

--- app/src/main/java/buaa/icourse/CourseActivity.java
+++ app/src/main/java/buaa/icourse/CourseActivity.java
@@ -2,6 +2,8 @@ package buaa.icourse;
 
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +11,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
 
 public class CourseActivity extends AppCompatActivity {
     public static final String TAG = "CourseActivity";
+    private EditText sid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +44,104 @@ public class CourseActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
-        Button courseCollect = findViewById(R.id.course_upload);
-        courseCollect.setOnClickListener(new View.OnClickListener() {
+        Intent intent = getIntent();
+        final String courseCode = intent.getStringExtra("Course_code");
+        final String courseName = intent.getStringExtra("Course_name");
+        final String courseCollege = intent.getStringExtra("Course_college");
+
+        Button courseUpload = findViewById(R.id.course_upload);
+
+        courseUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.e(TAG, "FUUUUUUUUUUUUUU");
                 Intent uploadIntent = new Intent(CourseActivity.this, UploadActivity.class);
-
-                Intent intent = getIntent();
-                String courseCode = intent.getStringExtra("Course_code");
-                String courseName = intent.getStringExtra("Course_name");
-                String courseCollege = intent.getStringExtra("Course_college");
-
                 Log.d(TAG, "********"+courseCode);
-
                 uploadIntent.putExtra("Course_code", courseCode);
                 uploadIntent.putExtra("Course_name", courseName);
                 uploadIntent.putExtra("Course_college", courseCollege);
-
                 startActivity(uploadIntent);
+            }
+        });
+
+        //课程收藏功能
+        final Button courseCollect = findViewById(R.id.course_collect);
+        final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        final String Sid = pref.getString("sid", "$$$");
+        Request request = new Request.Builder()
+                .url(UploadFragment.uploadUrl)
+                .post(new FormBody.Builder()
+                        .add("isCollect", "1")
+                        .add("studentNo", Sid)
+                        .add("Course_code", courseCode)
+                        .build()
+                ).build();
+
+        try {
+            OkHttpClient client = new OkHttpClient.Builder().
+                    connectTimeout(3, TimeUnit.SECONDS) //连接超时
+                    .readTimeout(4, TimeUnit.SECONDS) //读取超时
+                    .writeTimeout(5, TimeUnit.SECONDS) //写超时
+                    .build();
+
+            String responseString = client.newCall(request).execute().body().string();
+            System.out.println("@@" + responseString);
+            JSONArray jsonArray = new JSONArray(responseString);
+            JSONObject jo = jsonArray.getJSONObject(0);
+            //new JSONObject(responseString);
+            String res = jo.getString("res");
+            System.out.println("RES::::::" + res);
+            if (res.equals("yes")){
+                courseCollect.setText("取消收藏");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        courseCollect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String courseCollectText = (String)courseCollect.getText();
+                Log.e(TAG, "COOOOOOOOOO"+courseCollectText);
+                int toCollect = 2;
+                if (courseCollectText.equals("收藏该课程"))
+                    toCollect = 1;
+                Request request = new Request.Builder()
+                        .url(UploadFragment.uploadUrl)
+                        .post(new FormBody.Builder()
+                                .add("toCollect", ""+toCollect)
+                                .add("studentNo", Sid)
+                                .add("Course_code", courseCode)
+                                .build()
+                        ).build();
+
+                try {
+                    OkHttpClient client = new OkHttpClient.Builder().
+                            connectTimeout(3, TimeUnit.SECONDS) //连接超时
+                            .readTimeout(4, TimeUnit.SECONDS) //读取超时
+                            .writeTimeout(5, TimeUnit.SECONDS) //写超时
+                            .build();
+
+                    String responseString = client.newCall(request).execute().body().string();
+                    System.out.println("@@" + responseString);
+                    JSONArray jsonArray = new JSONArray(responseString);
+                    JSONObject jo = jsonArray.getJSONObject(0);
+                    //new JSONObject(responseString);
+                    String res = jo.getString("res");
+                    System.out.println("RES::::::" + res);
+                    if (res.equals("success")){
+                        if (toCollect == 1){
+                            courseCollect.setText("取消收藏");
+                        }else{
+                            courseCollect.setText("收藏该课程");
+                        }
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    //Toast.makeText(getApplicationContext(),
+                    //      "登录失败!请检查网络环境是否为校园网!", Toast.LENGTH_SHORT).show();
+                    return ;
+                }
             }
         });
     }
